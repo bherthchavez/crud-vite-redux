ef112d0ef1506f83a0b1a607a1491e5c89f9abd4
bherth <bherthchavez@gmail.com>
2023-06-01 22:50:03 +0800

    final with overflow

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,11 @@ function App() {
   })
 
   const haddleAddTask = () => {
-      setValueTask({ id: uuidv4(), name: '' })
-      dispatch(addTask(valueTask))
+    dispatch(addTask(valueTask))
+    setValueTask({ id: uuidv4(), name: '' })
   }
 
-  const handdleUpdateTask = ()=>{
+  const handdleUpdateTask = () => {
     dispatch(updateTask(valueTask))
     setUpdateId(null)
     setValueTask({ id: uuidv4(), name: '' })
@@ -29,17 +29,17 @@ function App() {
     setUpdateId(id)
   }
 
-  const hanndleDeleteTask = (id)=>{
+  const hanndleDeleteTask = (id) => {
     dispatch(deleteTask({ id }))
-    setValueTask({ id: '', name: '' })
+    setValueTask({ id: uuidv4(), name: '' })
     setUpdateId(null)
   }
 
   const renderCardTask = () => taskList.map(task => (
     <div key={task.id} className="flex justify-between p-4 bg-slate-800  rounded">
-      <div>
-      <h1 className="text-gray-300 text-left text-lg">{task.name} </h1>
-      <p className="text-gray-500 text-left text-xs">{task.id}</p>
+      <div className="text-left">
+        <h1 className="text-gray-300  text-lg">{task.name} </h1>
+        <p className="text-gray-500  text-xs">{task.id}</p>
       </div>
       <div className="flex gap-3">
         <button
@@ -59,7 +59,7 @@ function App() {
     <>
       <div className="h-screen bg-slate-950">
         <div className="grid justify-center gap-4 pt-20 text-center">
-          <div className="flex gap-2">
+          <div className="flex gap-2 p-3">
             <input
               type='text'
               onChange={(e) => setValueTask({ ...valueTask, name: e.target.value })}
@@ -67,14 +67,15 @@ function App() {
               className="py-2 px-3 font-medium rounded  focus:outline-none"
             />
             <button
-              onClick={updateId ? handdleUpdateTask  : haddleAddTask}
+              onClick={updateId ? handdleUpdateTask : haddleAddTask}
               className="bg-slate-700 w-32 text-gray-300  py-1 rounded hover:bg-slate-800"
             >
               {updateId ? 'Update Task' : ' Add Task'}
             </button>
           </div>
-
-          {taskList.length ? renderCardTask() : <p className="text-gray-300 pt-5"> No Task </p>}
+          <div className="grid gap-5 max-h-96 overflow-auto p-3">
+            {taskList.length ? renderCardTask() : <p className="text-gray-300 pt-5"> No Task </p>}
+          </div>
         </div>
       </div>
     </>
